2eebac156b23a232b5730002abc424c27aefe402
joel16 <joel16dev@gmail.com>
2026-01-02 19:42:43 +0000

    swagger: Expose a swagger page for manual testing

--- src/main/java/com/j16/ai/computer/vision/AiComputerVisionApplication.java
+++ src/main/java/com/j16/ai/computer/vision/AiComputerVisionApplication.java
@@ -1,8 +1,17 @@
 package com.j16.ai.computer.vision;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Azure AI Computer Vision Playground",
+				version = "1.0",
+				description = "API documentation for testing various Azure AI Java SDK features"
+		)
+)
 @SpringBootApplication
 public class AiComputerVisionApplication {
 
